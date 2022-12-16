e8c19f7f7997bb08bfd21ba0d0bdb37f2453fb9b
Frikk Odlolien <frikk@sentinel.no>
2022-12-16 12:57:23 +0100

    adds empty-state-page

--- src/components/empty-state.jsx
+++ src/components/empty-state.jsx
@@ -3,7 +3,7 @@ import { Tips } from "../assets/tips";
 export const EmptyState = (props) => {
   const { children } = props;
   return (
-    <div className="flex flex-col justify-center mt-20 bg-blue-50 justify-center items-center w-1/2 h-[200px] mx-auto rounded-lg">
+    <div className="flex flex-col justify-center mt-20 bg-blue-50 justify-center items-center w-1/2 h-[200px] mx-auto rounded-lg p-4">
       <div className="p-4 bg-blue-100 relative bottom-4 rounded-full">
         <Tips />
       </div>
